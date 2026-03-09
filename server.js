f49e2fb3e429aa0c3a09328ad3ece0e66ef3b034
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-03-09 11:35:20 +0100

    Change server log message format
    Update server log message to include 'http://localhost'

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on http://localhost${PORT}`);
+
 });
